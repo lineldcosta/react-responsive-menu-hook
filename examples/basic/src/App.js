9414c38b98a66b78604460e31ac0c5cfd2349b73
lineldcosta <lineldcosta@gmail.com>
2021-04-04 18:06:06 +0530

    Implementation of responsive menu

--- examples/basic/src/App.js
+++ examples/basic/src/App.js
@@ -1,12 +1,73 @@
 import React from 'react'
 import './App.css'
-import Button from '../../../dist/react-responsive-menu'
+import ReactResponsiveMenu from '../../../dist/react-responsive-menu'
+
+const style = {
+  minWidth: '100px',
+  padding: '4px',
+}
+
+const styles = {
+  menuWrapper: {
+    display: 'flex',
+    overflow: 'hidden',
+  },
+  overflowMenu: open => ({
+    display: 'flex',
+    flexDirection: 'column',
+    display: `${open ? 'inherit' : 'none'}`,
+    position: 'absolute',
+    right: '0px',
+  }),
+  overflowMenuWrapper: {
+    order: 99,
+    position: 'absolute',
+    right: '0',
+    background: '#fff',
+  },
+  overflowMenuIcon: {
+    textAlign: 'right',
+    marginRight: '15px',
+    cursor: 'pointer',
+  },
+}
 
 function App() {
   return (
-    <div className="App">
-      <Button></Button>
-    </div>
+    <ReactResponsiveMenu>
+      {props => {
+        console.log(props)
+        const { getMenuProps, showEllipsis, children, open, toggle } = props
+        return (
+          <React.Fragment>
+            <div {...getMenuProps({ id: 'Menu1', style })}>Home</div>
+            <div {...getMenuProps({ id: 'Menu2', style })}>Contact Us</div>
+            <div {...getMenuProps({ id: 'Menu3', style })}>About Us</div>
+            <div {...getMenuProps({ id: 'Menu8', style })}>Registration</div>
+            <div {...getMenuProps({ id: 'Menu9', style })}>Login</div>
+            <div {...getMenuProps({ id: 'Menu10', style })}>Help & Support</div>
+            <div {...getMenuProps({ id: 'Menu4', style })}>Blogs</div>
+            <div {...getMenuProps({ id: 'Menu5', style })}>Information</div>
+            <div {...getMenuProps({ id: 'Menu6', style })}>Apply</div>
+            <div {...getMenuProps({ id: 'Menu7', style })}>Cancel</div>
+
+            {showEllipsis && (
+              <div style={styles.overflowMenuWrapper}>
+                <div
+                  style={styles.overflowMenuIcon}
+                  onClick={() => toggle(true)}
+                >
+                  ...
+                </div>
+                <div id="long-menu" style={styles.overflowMenu(open)}>
+                  {children}
+                </div>
+              </div>
+            )}
+          </React.Fragment>
+        )
+      }}
+    </ReactResponsiveMenu>
   )
 }
 export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,145 @@
 import React from 'react'
+import classnames from 'classnames'
+import warning from 'warning'
 
-const Autocomplete = () => {
-  return <div>Testing Autocomplete 1</div>
+const hasSymbol = typeof Symbol === 'function' && Symbol.for
+
+const REACT_FRAGMENT_TYPE = hasSymbol ? Symbol.for('react.fragment') : 0xeacb
+
+const isFragment = object => object.type === REACT_FRAGMENT_TYPE
+
+const styles = {
+  menuWrapper: {
+    display: 'flex',
+    overflow: 'hidden',
+  },
 }
 
-export default Autocomplete
+const ReactResponsiveMenu = ({ children }) => {
+  const navRef = React.useRef(null)
+
+  const [menuItems, setMenuItems] = React.useState({})
+  const [open, setOpen] = React.useState(false)
+
+  const toggle = value => setOpen(value)
+
+  const observe = React.useCallback(
+    elements => {
+      const updatedEntries = {}
+      elements.forEach(element => {
+        const targetid = element.target.dataset.targetid
+        if (element.isIntersecting) {
+          updatedEntries[targetid] = true
+        } else {
+          updatedEntries[targetid] = false
+        }
+      })
+      setMenuItems(prev => ({
+        ...prev,
+        ...updatedEntries,
+      }))
+    },
+    [setMenuItems]
+  )
+
+  const attachObserver = React.useCallback(() => {
+    const observer = new IntersectionObserver(observe, {
+      root: navRef.current,
+      threshold: 1,
+    })
+
+    Array.from(navRef.current.children).forEach(item => {
+      if (item.dataset.targetid) {
+        observer.observe(item)
+      }
+    })
+    return observer
+  }, [])
+
+  const handleClickOutside = React.useCallback(
+    ({ target }) =>
+      navRef &&
+      navRef.current &&
+      !navRef.current.contains(target) &&
+      toggle(false),
+    [toggle]
+  )
+
+  React.useEffect(() => {
+    const observer = attachObserver()
+    document.addEventListener('click', handleClickOutside)
+    return () => {
+      observer.disconnect()
+      document.removeEventListener('click', handleClickOutside)
+    }
+  }, [])
+
+  // React.useEffect(() => {
+  //   warning(!isFragment(children()), `Menu should wrapped inside fragment`)
+  // }, [children])
+
+  const showEllipsis = React.useMemo(
+    () => Object.values(menuItems).some(v => v === false),
+    [menuItems]
+  )
+
+  const overFlowMenu = child => {
+    if (
+      !child ||
+      !child.props['data-targetid'] ||
+      menuItems[child.props['data-targetid']]
+    )
+      return null
+    return (
+      <React.Fragment key={child}>
+        {React.cloneElement(child, {
+          onClick: () => toggle(false),
+        })}
+      </React.Fragment>
+    )
+  }
+
+  const getMenuProps = menuType => ({ id, ...props } = {}) => ({
+    'data-targetid': id,
+    className: classnames({
+      visible:
+        (menuType === 'menu' && !!menuItems[id]) ||
+        (menuType === 'overflowMenu' && !menuItems[id]),
+      inVisible:
+        (menuType === 'menu' && !menuItems[id]) ||
+        (menuType === 'overflowMenu' && !!menuItems[id]),
+    }),
+    onClick: () => menuType === 'overflowMenu' && toggle(false),
+    ...props,
+  })
+
+  const flattenChildren = children =>
+    isFragment(children)
+      ? children.props.children.filter(
+          child => child && child.props['data-targetid']
+        )
+      : children
+
+  const childProps = {
+    getMenuProps: getMenuProps('menu'),
+    showEllipsis,
+    children: React.Children.map(
+      flattenChildren(
+        children({
+          getMenuProps: getMenuProps('overflowMenu'),
+        })
+      ),
+      overFlowMenu
+    ),
+    open,
+    toggle,
+  }
+
+  return (
+    <div ref={navRef} style={styles.menuWrapper}>
+      {children({ ...childProps })}
+    </div>
+  )
+}
+
+export default ReactResponsiveMenu
